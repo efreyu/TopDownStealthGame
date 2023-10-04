23e91e0a5475f7b31e097e850c9f4cb7c6814354
Artur Barilo <efreyu@gmail.com>
2023-10-04 21:22:24 +0300

    master | base player movement and add tileset /wip

--- TopDownStealthProject/scripts/player.gd
+++ TopDownStealthProject/scripts/player.gd
@@ -9,16 +9,18 @@ func _process(delta):
 
 
 func update_motion(delta):
+	look_at(get_global_mouse_position())
+	
 	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
 		velocity.y = clamp((velocity.y - SPEED), -MAX_SPEED, 0)
-	if Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
 		velocity.y = clamp((velocity.y + SPEED), 0, MAX_SPEED)
 	else:
 		velocity.y = lerp(velocity.y, 0.0, FRICTION)
 		
 	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		velocity.x = clamp((velocity.x - SPEED), -MAX_SPEED, 0)
-	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		velocity.x = clamp((velocity.x + SPEED), 0, MAX_SPEED)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, FRICTION)
